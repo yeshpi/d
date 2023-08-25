8abd1d3805ab58bb3c853a2ad656bffbb900e659
yeshpi <59283010+yeshpi@users.noreply.github.com>
2023-08-24 23:45:51 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ namespace testtv{
     //%blockId="testOn"
     //%block="plot $x ,$y"
     //%x.min=0 x.max=5
-    //%$y.min=0 $y.max=5
+    //%y.min=0 y.max=5
     export function ledOn(x:number,y:number):void{
         
         led.plot(x, y)
